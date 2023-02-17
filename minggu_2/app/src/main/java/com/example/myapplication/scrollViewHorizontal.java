ef47e32e29adf41e28bde089e239a0bd9fc94a15
fillah23 <fillahseptian7l24@gmail.com>
2023-02-17 14:20:13 +0700

    minggu2

---
+++ minggu_2/app/src/main/java/com/example/myapplication/scrollViewHorizontal.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class scrollViewHorizontal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroll_view_horizontal);
+    }
+}
